e7d4ff2b346888ead371eb2bd42de14adc4694f9
Nadeko Sengoku <nadeko@z-solutions.co.jp>
2026-02-13 13:25:55 +0900

    Bump to 1.1.0 for update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "de.koshi.photodream"
         minSdk = 23
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.1"
+        versionCode = 2
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
