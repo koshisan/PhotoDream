0291fa093eb7e92b8513f0a9f0be5a74d43ae009
Nadeko Sengoku <nadeko@z-solutions.co.jp>
2026-02-14 21:18:35 +0900

    chore: Bump version to 1.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "de.koshi.photodream"
         minSdk = 23
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.3.1"
+        versionCode = 6
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
